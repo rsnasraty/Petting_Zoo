c3b23d7d0360927f41edcc4c62ab8d0c5291afa0
Roxanne Nasraty <rsnasraty@gmail.com>
2020-07-15 14:53:05 -0500

    Practice: Classy Critters Collection

---
+++ animals.py
@@ -0,0 +1,239 @@
+# import the python datetime module to help us create a timestamp
+from datetime import date
+
+# 1
+
+
+class Hognose_Snake:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.slithering = True
+
+
+snakey = Hognose_Snake()
+
+# 2
+
+
+class Cobra:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.slithering = True
+
+
+mister_cobra = Cobra()
+
+# 3
+
+
+class Python:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.slithering = True
+
+
+monty = Python()
+
+# 4
+
+
+class Boa_Constrictor:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.slithering = True
+
+
+b_sizzle = Boa_Constrictor()
+
+# 5
+
+
+class Corn_Snake:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.slithering = True
+
+
+chip = Corn_Snake()
+
+# 6
+
+
+class Emu:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.walking = True
+
+
+googles = Emu()
+
+# 7
+
+
+class Llama:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.walking = True
+
+
+kuzco = Llama()
+# 8
+
+
+class Donkey:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.walking = True
+
+
+shrek = Donkey()
+
+# 9
+
+
+class Goat:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.walking = True
+
+
+billy = Goat()
+
+# 10
+
+
+class Zebra:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.walking = True
+
+
+z_man = Zebra()
+
+# 11
+
+
+class Mallard:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.swimming = True
+
+
+quackers = Mallard()
+# 12
+
+
+class Goldfish:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.swimming = True
+
+
+goldeen = Goldfish()
+
+# 13
+
+
+class Swan:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.swimming = True
+
+
+odette = Swan()
+
+# 14
+
+
+class Koi:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.swimming = True
+
+
+jinli = Koi()
+# 15
+
+
+class Goose:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.swimming = True
+
+
+untitled = Goose()
