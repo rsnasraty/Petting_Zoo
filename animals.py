84d380d1facc7d7b54262de2e1057b52b1a9d41a
Roxanne Nasraty <rsnasraty@gmail.com>
2020-07-16 15:36:58 -0500

    Finished

--- animals.py
+++ animals.py
@@ -17,13 +17,14 @@ class Hognose_Snake:
 
     def feed(self):
         print(f'{self.name} was fed {self.food} on   {date.today().strftime("%m/%d/%Y")}.')
-
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
 
 snakey = Hognose_Snake("Snakey","Hognose Snake", "Mice")
+print(snakey)
 print(snakey.feed())
 
 # 2
-
 
 class Cobra:
 
@@ -38,9 +39,11 @@ class Cobra:
 
     def feed(self):
         print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}.')
-
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
 
 mister_cobra = Cobra("Mister Cobra", "Cobra", "Mice")
+print(mister_cobra)
 print(mister_cobra.feed())
 
 # 3
@@ -59,8 +62,11 @@ class Python:
 
     def feed(self):
         print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}.')
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
 
 monty = Python("Monty", "Python", "Mice")
+print(monty)
 print(monty.feed())
 
 # 4
@@ -79,8 +85,10 @@ class Boa_Constrictor:
 
     def feed(self):
         print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}.')
-
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
 b_sizzle = Boa_Constrictor("B-Sizzle", "Boa Constrictor", "Mice")
+print(b_sizzle)
 print(b_sizzle.feed())
 
 # 5
@@ -99,8 +107,10 @@ class Corn_Snake:
 
     def feed(self):
         print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}.')
-
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
 chip = Corn_Snake("Chip", "Corn Snake", "Mice")
+print(chip)
 print(chip.feed())
 
 # 6
@@ -120,9 +130,11 @@ class Emu:
 
     def feed(self):
         print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}.')
-
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
 googles = Emu("Googles", "Emu", "morning", "Emu Chow")
 print(f'{googles.name} the {googles.species} is available to pet during the {googles.shift} shift.')
+print(googles)
 print(googles.feed())
 # 7
 
@@ -141,10 +153,12 @@ class Llama:
 
     def feed(self):
         print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}.')
-
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
 kuzco = Llama("Kuzco", "Llama", "Afternoon", "Fruit")
 
 print(f'{kuzco.name} the {kuzco.species} is available to pet during the {kuzco.shift} shift.')
+print(kuzco)
 print(kuzco.feed())
 
 
@@ -165,9 +179,11 @@ class Donkey:
 
     def feed(self):
         print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}.')
-
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
 shrek = Donkey("Shrek", "Donkey", "morning", "pasta")
 print(f'{shrek.name} the {shrek.species} is available to pet during the {shrek.shift} shift.')
+print(shrek)
 print(shrek.feed())
 
 # 9
@@ -187,9 +203,11 @@ class Goat:
 
     def feed(self):
         print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}.')
-
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
 billy = Goat("Billy", "Goat", "Afternoon", "Grass")
 print(f'{billy.name} the {billy.species} is available to pet during the {billy.shift} shift.')
+print(billy)
 print(billy.feed())
 
 # 10
@@ -209,9 +227,12 @@ class Zebra:
 
     def feed(self):
         print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}.')
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
 
 z_man = Zebra("Z-Man", "Zebra", "afternoon", "Berries")
 print(f'{z_man.name} the {z_man.species} is available to pet during the {z_man.shift} shift.')
+print(z_man)
 print(z_man.feed())
 
 # 11
@@ -230,11 +251,13 @@ class Mallard:
 
     def feed(self):
         print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}.')
-
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
 quackers = Mallard("Quackers", "Duck", "Bread")
-# 12
+print(quackers)
 print(quackers.feed())
 
+#12
 class Goldfish:
 
     def __init__(self, name, species, food):
@@ -248,9 +271,11 @@ class Goldfish:
 
     def feed(self):
         print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}.')
-
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
 
 goldeen = Goldfish("Goldeen", "Goldfish", "Fish Food")
+print(goldeen)
 print(goldeen.feed())
 
 # 13
@@ -269,9 +294,11 @@ class Swan:
 
     def feed(self):
         print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}.')
-
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
 
 odette = Swan("Odette", "Swan", "Swan Snacks")
+print(odette)
 print(odette.feed())
 
 # 14
@@ -291,8 +318,11 @@ class Koi:
     def feed(self):
         print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}.')
 
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
 
 jinli = Koi("Jinli", "Koi", "Koi Krunchies")
+print(jinli)
 print(jinli.feed())
 # 15
 
@@ -311,5 +341,9 @@ class Goose:
     def feed(self):
         print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}.')
 
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
+
 untitled = Goose("Untitled", "Goose", "Bread")
+print(untitled)
 print(untitled.feed())
