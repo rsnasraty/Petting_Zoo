bf09fe697f8194aeb0fbaa26cfe2d0e930f392d2
Roxanne Nasraty <rsnasraty@gmail.com>
2020-07-16 14:08:42 -0500

    Completed

--- animals.py
+++ animals.py
@@ -9,8 +9,8 @@ class Hognose_Snake:
     def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.slithering = True
 
@@ -25,8 +25,8 @@ class Cobra:
     def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.slithering = True
 
@@ -41,8 +41,8 @@ class Python:
     def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.slithering = True
 
@@ -57,8 +57,8 @@ class Boa_Constrictor:
     def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.slithering = True
 
@@ -73,8 +73,8 @@ class Corn_Snake:
     def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.slithering = True
 
@@ -86,79 +86,90 @@ chip = Corn_Snake("Chip", "Corn Snake")
 
 class Emu:
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.walking = True
 
+        self.shift = shift
 
-googles = Emu("Googles", "Emu")
-
+googles = Emu("Googles", "Emu", "morning")
+print(f'{googles.name} the {googles.species} is available to pet during the {googles.shift} shift.')
 # 7
 
 
 class Llama:
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.walking = True
+        self.shift = shift
 
 
-kuzco = Llama("Kuzco", "Llama")
+kuzco = Llama("Kuzco", "Llama", "Afternoon")
+
+print(f'{kuzco.name} the {kuzco.species} is available to pet during the {kuzco.shift} shift.')
+
+
+
 # 8
 
 
 class Donkey:
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.walking = True
+        self.shift = shift
 
-
-shrek = Donkey("Shrek", "Donkey")
+shrek = Donkey("Shrek", "Donkey", "morning")
+print(f'{shrek.name} the {shrek.species} is available to pet during the {shrek.shift} shift.')
 
 # 9
 
 
 class Goat:
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.walking = True
+        self.shift = shift
 
 
-billy = Goat("Billy", "Goat")
+billy = Goat("Billy", "Goat", "Afternoon")
+print(f'{billy.name} the {billy.species} is available to pet during the {billy.shift} shift.')
 
 # 10
 
 
 class Zebra:
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.walking = True
+        self.shift = shift
 
-
-z_man = Zebra("Z-Man", "Zebra")
+z_man = Zebra("Z-Man", "Zebra", "afternoon")
+print(f'{z_man.name} the {z_man.species} is available to pet during the {z_man.shift} shift.')
 
 # 11
 
@@ -168,8 +179,8 @@ class Mallard:
     def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.swimming = True
 
@@ -183,8 +194,8 @@ class Goldfish:
     def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.swimming = True
 
@@ -199,8 +210,8 @@ class Swan:
     def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.swimming = True
 
@@ -215,8 +226,8 @@ class Koi:
     def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.swimming = True
 
@@ -230,8 +241,8 @@ class Goose:
     def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.swimming = True
 
